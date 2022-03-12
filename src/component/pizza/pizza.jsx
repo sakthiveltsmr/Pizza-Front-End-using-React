dabe45fc354caa275a2356f583257ceb23001357
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-12 15:44:34 +0530

    pizza

--- src/component/pizza/pizza.jsx
+++ src/component/pizza/pizza.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function pizza() {
-  return <div>pizza</div>;
+export default function pizza({ pizza }) {
+  return (
+    <div>
+      <h1>{pizza.name}</h1>
+      <img src={pizza.image} alt="img" className="img-fluid" />
+    </div>
+  );
 }

b86100612bc18729b7e5496c90367527d36e8391
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-12 15:10:11 +0530

    pizza

---
+++ src/pizzadata.js
@@ -0,0 +1,104 @@
+[
+  {
+    name: "PEPPER BARBECUE CHICKEN",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    description: "Papper Barbecue Chicken I cheese",
+  },
+  {
+    name: "",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    description: "Papper Barbecue Chicken I cheese",
+  },
+  {
+    name: "CHICKEN SAUSAGE",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Chicken_Sausage.jpg",
+    description: "Chicken Sausage I Cheese",
+  },
+  {
+    name: "PEPPY PANEER",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg",
+    description:
+      "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!",
+  },
+  {
+    name: "INDI TANDOORI PANEER",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/IndianTandooriPaneer.jpg",
+    description:
+      "It is hot. It is spicy. It is oh-so-Indian. Tandoori paneer with capsicum I red paprika I mint mayo",
+  },
+  {
+    name: "VEGGIE PARADISE",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "veg",
+    image:
+      "https://www.dominos.co.in/files/items/Digital_Veggie_Paradise_olo_266x265.jpg",
+    description: "Goldern Corn, Black Olives, Capsicum & Red Paprika",
+  },
+  {
+    name: "CHICKEN FIESTA",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/chunky-chicken.png",
+    description:
+      "Grilled Chicken Rashers I Peri-Peri Chicken I Onion I Capsicum",
+  },
+];

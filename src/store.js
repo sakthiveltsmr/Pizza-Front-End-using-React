70eb0c451fe6f4793b186198fc3498e83c395283
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-15 21:30:15 +0530

    store

--- src/store.js
+++ src/store.js
@@ -1,1 +1,18 @@
-import combineReducers from "redux";
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { getAllPizzasReducer } from "./reducer/reducer";
+const finalReducer = combineReducers({
+  getAllPizzasReducer: getAllPizzasReducer,
+});
+
+const initialState = {};
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(
+  finalReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;

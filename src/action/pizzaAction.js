b49b90ceb7433284532cf7ca25ba683f5ebd3705
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-15 22:44:31 +0530

    pizzas

--- src/action/pizzaAction.js
+++ src/action/pizzaAction.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAllPizzas = () => async (dispatch) => {
   dispatch({ type: "GET_PIZZAS_REQUEST" });
   try {
-    const response = await axios.get("/api/pizzas/getallpizzas");
+    const response = await axios.get("/api/pizza/getallpizzas");
     console.log(response);
     dispatch({ type: "GET_PIZZAS_SUCCESS", payload: response.data });
   } catch (error) {

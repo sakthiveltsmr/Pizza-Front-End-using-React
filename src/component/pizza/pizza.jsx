03885f48e357d4b1d3135a7b635630cfac906410
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-15 22:29:07 +0530

    pizza

--- src/component/pizza/pizza.jsx
+++ src/component/pizza/pizza.jsx
@@ -51,7 +51,7 @@ export default function Pizza({ pizza }) {
       <div className="flex-container">
         <div className="m-1 w-100">
           <h1 className="mt-1">
-            Price:{pizza.prices[0][varient] * [quantity]}/-
+            Price:₹{pizza.prices[0][varient] * [quantity]}/-
           </h1>
         </div>
         <div className="m-1 w-100">

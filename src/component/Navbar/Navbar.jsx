d4924b422975ee0247104077b4571e8685c7fbb6
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-16 21:03:56 +0530

    nav

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 // import "../../index.css";
+import { useDispatch, useSelector } from "react-redux";
 function Navbar() {
+  const cartstate = useSelector((state) => state.cartReducer);
   return (
     <div>
       <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded ">
@@ -30,7 +32,7 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Carts
+                Cart {cartstate.cartItems.length}
               </a>
             </li>
           </ul>

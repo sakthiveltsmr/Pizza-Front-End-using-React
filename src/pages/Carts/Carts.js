d553c0c5e1fae738553e00b049660f026381276f
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-17 16:16:13 +0530

    cart

--- src/pages/Carts/Carts.js
+++ src/pages/Carts/Carts.js
@@ -1,33 +1,98 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-export default function Carts() {
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../../action/cartAction";
+import { deleteFromCart } from "../../action/cartAction";
+import Checkout from "../components/Checkout";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Navigate } from "react-router-dom";
+export default function Cartscreen() {
+  AOS.init();
+  const [redirect, setRedirect] = useState(false);
   const cartstate = useSelector((state) => state.cartReducer);
   const cartItems = cartstate.cartItems;
+  var subtotal = cartItems.reduce((x, item) => x + item.price, 0);
+  const dispatch = useDispatch();
+
+  const orderstate = useSelector((state) => state.placeOrderReducer);
+  const { success } = orderstate;
+
+  if (success) {
+    localStorage.removeItem("cartItems");
+    cartItems.map((item) => {
+      dispatch(deleteFromCart(item));
+    });
+    setTimeout(() => {
+      setRedirect(true);
+    }, 2000);
+  }
+  if (redirect) {
+    return <Redirect to="/orders" />;
+  }
   return (
     <div>
-      <div className="row justify-content-center">
+      <div className="row justify-content-center p-2" data-aos="fade-down">
         <div className="col-md-6">
-          <h2 style={{ fontSize: "30px" }}>My Cart</h2>
+          <h2 style={{ fontSize: "40px" }}>My Cart</h2>
+
           {cartItems.map((item) => {
-            console.log(item);
             return (
               <div className="flex-container">
-                <div>
-                  <h1>{item.name}</h1>
+                <div className="text-left m-1 w-100">
                   <h1>
-                    Price:{item.quantity}*{item.prices[0][item.varient]}=
+                    {item.name} [{item.varient}]
+                  </h1>
+                  <h1>
+                    Price : {item.quantity} * {item.prices[0][item.varient]} ={" "}
                     {item.price}
                   </h1>
+                  <h1 style={{ display: "inline" }}>Quantity : </h1>
+                  <i
+                    className="fa fa-plus"
+                    aria-hidden="true"
+                    onClick={() => {
+                      dispatch(
+                        addToCart(item, item.quantity + 1, item.varient)
+                      );
+                    }}
+                  ></i>
+                  <b>{item.quantity}</b>
+                  <i
+                    className="fa fa-minus"
+                    aria-hidden="true"
+                    onClick={() => {
+                      dispatch(
+                        addToCart(item, item.quantity - 1, item.varient)
+                      );
+                    }}
+                  ></i>
+                  <hr />
                 </div>
-                <div>
-                  <img src={item.image} />
+
+                <div className="m-1 w-100">
+                  <img
+                    src={item.image}
+                    style={{ height: "80px", height: "80px" }}
+                  />
                 </div>
-                <div></div>
+                <div className="m-1 w-100">
+                  <i
+                    className="fa fa-trash mt-5"
+                    aria-hidden="true"
+                    onClick={() => {
+                      dispatch(deleteFromCart(item));
+                    }}
+                  ></i>
+                </div>
               </div>
             );
           })}
         </div>
-        <div className="col-md-4"></div>
+
+        <div className="col-md-4 text-right">
+          <h2 style={{ fontSize: "45px" }}>SubTotal : {subtotal} /-</h2>
+          <Checkout subtotal={subtotal} />
+        </div>
       </div>
     </div>
   );

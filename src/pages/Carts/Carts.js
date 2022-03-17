2d413068a8477a62421186540c6c75252e9dce62
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-17 13:53:37 +0530

    carts

--- src/pages/Carts/Carts.js
+++ src/pages/Carts/Carts.js
@@ -1,5 +1,13 @@
 import React from "react";
-
+import { useDispatch, useSelector } from "react-redux";
 export default function Carts() {
-  return <div>Carts</div>;
+  const cartstate = useSelector((state) => state.cartReducer);
+  return (
+    <div>
+      <div class="row justify-content-center">
+        <div className="col-md-6"></div>
+        <div className="col-md-4"></div>
+      </div>
+    </div>
+  );
 }

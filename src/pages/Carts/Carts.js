d0e029f843dd84b4424ca14a25b0a9cbf86ed928
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-17 14:20:56 +0530

    carts

--- src/pages/Carts/Carts.js
+++ src/pages/Carts/Carts.js
@@ -6,7 +6,24 @@ export default function Carts() {
   return (
     <div>
       <div class="row justify-content-center">
-        <div className="col-md-6"></div>
+        <div className="col-md-6">
+          <h2 style={{ fontSize: "30px" }}>My Cart</h2>
+          {cartItems.map((item) => {
+            console.log(item);
+            return (
+              <div className="flex-container">
+                <div>
+                  <h1>{item.name}</h1>
+                  <h1>
+                    Price:{item.quantity} * {item.varient}={item.price}
+                  </h1>
+                </div>
+                <div></div>
+                <div></div>
+              </div>
+            );
+          })}
+        </div>
         <div className="col-md-4"></div>
       </div>
     </div>

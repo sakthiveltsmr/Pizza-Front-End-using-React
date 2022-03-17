597026ca98fd19431358383dea9611b2d62e6dbc
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-17 13:40:37 +0530

    app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./component/Navbar/Navbar";
 import Home from "./pages/Homepage/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

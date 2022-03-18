967bff4bab7ccd4b2bc63186188da960be7998d4
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-18 13:52:52 +0530

    nav

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         >
           <ul className="navbar-nav mr-auto">
             <li className="nav-item ">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/login">
                 Login
               </a>
             </li>

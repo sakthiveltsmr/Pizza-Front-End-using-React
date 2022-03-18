0bec2c69b060f065b4fe3998672fa0786cbc86e2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-18 14:12:44 +0530

    register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,35 +1,90 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../../action/userActions";
+import Error from "../../component/Error/Error";
+import Loading from "../../component/loading/Loading";
+import Success from "../../component/Success/Success";
 export default function Register() {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [cpassword, setcpassword] = useState("");
+  const registerstate = useSelector((state) => state.registerUserReducer);
+  const { error, loading, success } = registerstate;
+  const dispatch = useDispatch();
+  function register() {
+    if (password != cpassword) {
+      alert("passwords not matched");
+    } else {
+      const user = {
+        name,
+        email,
+        password,
+      };
+      console.log(user);
+      dispatch(registerUser(user));
+    }
+  }
+
   return (
-    <div>
-      <div class="row justify-content-center mt-5">
-        <div className="col-md-5 mt-5">
+    <div className="register">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
+          {loading && <Loading />}
+          {success && <Success success="User Registered Successfully" />}
+          {error && <Error error="Email already registred" />}
+
+          <h2 className="text-center m-2" style={{ fontSize: "35px" }}>
+            Register
+          </h2>
           <div>
             <input
+              required
               type="text"
               placeholder="name"
               className="form-control"
-              required
+              value={name}
+              onChange={(e) => {
+                setname(e.target.value);
+              }}
             />
             <input
+              required
               type="email"
               placeholder="email"
               className="form-control"
-              required
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
             />
             <input
-              type="text"
+              type="password"
               placeholder="password"
               className="form-control"
+              value={password}
               required
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
             />
             <input
               type="text"
               placeholder="confirm password"
               className="form-control"
+              value={cpassword}
               required
+              onChange={(e) => {
+                setcpassword(e.target.value);
+              }}
             />
+            <button onClick={register} className="btn mt-3 mb-3">
+              REGISTER
+            </button>
+            <br />
+            <a style={{ color: "black" }} href="/login">
+              Click Here To Login
+            </a>
           </div>
         </div>
       </div>

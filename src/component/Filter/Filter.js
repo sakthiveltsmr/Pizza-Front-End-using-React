5b972d3141028a0ffd0a5fb8ee4404f8f23e683b
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-19 21:44:20 +0530

    filter

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Filter() {
   return (
     <div className="container">
-      <div class="row justify-content-center shadow-lg p-3 mb-5 bg-white rounded "></div>
+      <div class="row justify-content-center shadow-lg p-3 mb-5 bg-white rounded ">
+        <div className="col-md-3 w-100">
+          <input
+            onChange={(e) => {
+              setsearchkey(e.target.value);
+            }}
+            value={searchkey}
+            type="text"
+            className="form-control w-100"
+            placeholder="search your dishes"
+          />
+        </div>
+      </div>
     </div>
   );
 }

a1fe7adb55f6a94f8c1136e74adaed357314ad36
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-19 22:43:42 +0530

    filter

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { filterPizzas } from "../../action/pizzaAction";
-
 export default function Filter() {
   const dispatch = useDispatch();
   const [searchkey, setsearchkey] = useState("");
   const [category, setcategory] = useState("all");
   return (
-    <div className="container filter">
-      <div class="row justify-content-center shadow-lg p-3 mb-5 bg-white rounded ">
-        <div className="col-md-3 w-100">
+    <div className="container">
+      <div className="row justify-content-center shadow-lg p-3 mb-5 bg-white rounded">
+        <div className="col-md-3 w-32">
           <input
             onChange={(e) => {
               setsearchkey(e.target.value);
@@ -17,30 +16,28 @@ export default function Filter() {
             value={searchkey}
             type="text"
             className="form-control w-100"
-            placeholder="search your dishes"
+            placeholder="search pizzas"
           />
         </div>
-        <div className="col-md-3 w-100">
+        <div className="col-md-3 w-32">
           <select
             className="form-control w-100 mt-2"
             value={category}
-            onChange={(e) => {
-              setcategory(e.target.value);
-            }}
+            onChange={(e) => setcategory(e.target.value)}
           >
             <option value="all">All</option>
             <option value="veg">Veg</option>
             <option value="nonveg">Non Veg</option>
           </select>
         </div>
-        <div className="col-md-3 w-100">
+        <div className="col-md-3 w-32">
           <button
-            className="btn btn-info w-100 mt-2"
+            className="btn btn-info w-50 mt-2"
             onClick={() => {
               dispatch(filterPizzas(searchkey, category));
             }}
           >
-            FILTER
+            Search
           </button>
         </div>
       </div>

4c088256ef84a9f6c9cab340aab3128723dd103a
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-19 23:20:33 +0530

    app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Carts from "./pages/Carts/Carts";
 import Register from "./pages/Register/Register";
 import Login from "./pages/login/login";
+import Order from "./pages/Order/Order";
+import Admin from "./pages/Admin/Admin";
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,8 @@ function App() {
           <Route path="/cart" exact element={<Carts />} />
           <Route path="/register" exact element={<Register />} />
           <Route path="/login" exact element={<Login />} />
+          <Route path="/orders" exact element={<Order />} />
+          <Route path="/admin" exact element={<Admin />} />
         </Routes>
       </Router>
     </div>

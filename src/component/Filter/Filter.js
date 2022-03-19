1421cfa9f4488c1fbc2b293fb6e4772e5798caff
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-19 21:37:55 +0530

    filter

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Filter() {
-  return <div>Filter</div>;
+  return (
+    <div className="container">
+      <div class="row justify-content-center shadow-lg p-3 mb-5 bg-white rounded "></div>
+    </div>
+  );
 }

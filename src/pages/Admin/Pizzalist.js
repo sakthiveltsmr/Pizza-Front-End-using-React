996efa6da22f21aac5339770197c1ab62ed9a187
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-20 20:37:08 +0530

    pizza

--- src/pages/Admin/Pizzalist.js
+++ src/pages/Admin/Pizzalist.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { deletePizza, getAllPizzas } from "../../action/pizzaAction";
 
 export default function Pizzalist() {
   return (
@@ -14,6 +17,16 @@ export default function Pizzalist() {
             <th>Actions</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>name</td>
+            <td>
+              Small:{Pizza.prices[0]["small"]}
+              <br />
+              Medium:{Pizza}
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );

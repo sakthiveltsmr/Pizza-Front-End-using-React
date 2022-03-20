82a822458b3cc639eb5d23227731c627f7ef9656
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-20 20:32:40 +0530

    pizza

--- src/pages/Admin/Pizzalist.js
+++ src/pages/Admin/Pizzalist.js
@@ -4,6 +4,17 @@ export default function Pizzalist() {
   return (
     <div>
       <h2>Pizzalist</h2>
+
+      <table className="table table-bordered table-responsive-sm">
+        <thead className="thead-dark">
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }

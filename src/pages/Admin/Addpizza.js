65db5f10e2178533182354036588c1d6baf27155
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-20 21:33:12 +0530

    addpizza

--- src/pages/Admin/Addpizza.js
+++ src/pages/Admin/Addpizza.js
@@ -43,6 +43,74 @@ export default function Addpizza() {
         {loading && <Loading />}
         {error && <Error error="something went wrong" />}
         {success && <Success success="New Piza added Successfully" />}
+        <form onSubmit={Formhandler}>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="name"
+            value={name}
+            onChange={(e) => {
+              setname(e.target.value);
+            }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="small varient price"
+            value={smallprice}
+            onChange={(e) => {
+              setsmallprice(e.target.value);
+            }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="medium varient price"
+            value={mediumprice}
+            onChange={(e) => {
+              setmediumprice(e.target.value);
+            }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="large varient price"
+            value={largeprice}
+            onChange={(e) => {
+              setlargeprice(e.target.value);
+            }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="category"
+            value={category}
+            onChange={(e) => {
+              setcategory(e.target.value);
+            }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="description"
+            value={description}
+            onChange={(e) => {
+              setdescription(e.target.value);
+            }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="image url"
+            value={image}
+            onChange={(e) => {
+              setimage(e.target.value);
+            }}
+          />
+          <button className="btn mt-3" type="submit">
+            Add Pizza
+          </button>
+        </form>
       </div>
     </div>
   );

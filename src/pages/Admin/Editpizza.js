167b0deb98130a59b92f3e8b4dcc42065a4ea9c2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-22 20:59:27 +0530

    edit

---
+++ src/pages/Admin/Editpizza.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { editPizza, getPizzaById } from "../../action/pizzaAction";
+export default function Editpizza() {
+  return <div>Editpizza</div>;
+}

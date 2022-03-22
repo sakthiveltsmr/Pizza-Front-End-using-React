2004d2f6089be75eb7f80646571b431ec368a205
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-22 21:05:34 +0530

    editpizza

--- src/pages/Admin/Editpizza.js
+++ src/pages/Admin/Editpizza.js
@@ -2,5 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { editPizza, getPizzaById } from "../../action/pizzaAction";
 export default function Editpizza() {
+  const dispatch = useDispatch();
+  const [name, setname] = useState("");
+  const [smallprice, setsmallprice] = useState();
+  const [mediumprice, setmediumprice] = useState();
+  const [largeprice, setlargeprice] = useState();
+  const [image, setimage] = useState("");
+  const [description, setdescription] = useState("");
+  const [category, setcategory] = useState("");
   return <div>Editpizza</div>;
 }

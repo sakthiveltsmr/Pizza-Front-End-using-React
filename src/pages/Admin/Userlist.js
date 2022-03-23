8512a92b219071adfec5bde1f6004ae34ff56702
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-23 21:07:35 +0530

    userlist

--- src/pages/Admin/Userlist.js
+++ src/pages/Admin/Userlist.js
@@ -19,6 +19,37 @@ export default function Userslist() {
       <h1>Users list</h1>
       {loading && <Loading />}
       {error && <Error error="Something went wrong" />}
+      <table className="table table-striped table-bordered table-responsive-sm">
+        <thead className="thead-dark">
+          <tr>
+            <th>User Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users &&
+            users.map((user) => {
+              return (
+                <tr>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <i
+                      className="fa fa-trash"
+                      onClick={() => {
+                        dispatch(deleteUser(user._id));
+                      }}
+                    ></i>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }

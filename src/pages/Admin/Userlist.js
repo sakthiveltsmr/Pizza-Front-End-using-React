2b4b5b45dc06adbffb789224a62d09c2c3497b5e
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-23 20:57:38 +0530

    userslist

--- src/pages/Admin/Userlist.js
+++ src/pages/Admin/Userlist.js
@@ -4,3 +4,9 @@ import { getAllOrders } from "../../action/orderActions";
 import Error from "../../component/Error/Error";
 import Filter from "../../component/Filter/Filter";
 import Loading from "../../component/loading/Loading";
+import { deleteUser, getAllUsers } from "../../action/userActions";
+
+export default function Userslist() {
+  const dispatch = useDispatch();
+  const usersstate = useSelector((state) => state.getAllUsersReducer);
+}

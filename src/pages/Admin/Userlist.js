98be01a220a457b1a013b014e96ac67f819837c2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-23 21:01:57 +0530

    user

--- src/pages/Admin/Userlist.js
+++ src/pages/Admin/Userlist.js
@@ -9,4 +9,16 @@ import { deleteUser, getAllUsers } from "../../action/userActions";
 export default function Userslist() {
   const dispatch = useDispatch();
   const usersstate = useSelector((state) => state.getAllUsersReducer);
+  const { error, loading, users } = usersstate;
+
+  useEffect(() => {
+    dispatch(getAllUsers());
+  }, []);
+  return (
+    <div>
+      <h1>Users list</h1>
+      {loading && <Loading />}
+      {error && <Error error="Something went wrong" />}
+    </div>
+  );
 }
